4a0e2cd4ec239d38a3cab3c9883b7eba57bc23e2
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-06 17:08:57 +0200

    add path
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -7,13 +7,28 @@ class sensu::packages {
 	'Debian': {
 		$require = Apt::Source['sensu']
 	}
-}
+   }
 
    package {
 	"sensu":
 		require => $require,
 		ensure => "latest"; 
    }
+
+   file {
+    	"/etc/sensu/extensions/handlers/":
+		ensure 	=> present,
+		recurse	=> true,
+		require => Package['sensu'],
+		source	=> "puppet:///modules/sensu/handlers/";
+ 	"/etc/sensu/extensions/mutators/":
+		ensure 	=> present,
+		recurse	=> true,
+		require => Package['sensu'],
+		source	=> "puppet:///modules/sensu/mutators/";
+
+   }
+
 
 }
 
