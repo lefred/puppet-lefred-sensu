5f9cac43fbceac18f17e53184156a0a6f637fc03
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-13 12:01:10 +0200

    add interval
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,8 @@
 class sensu::config {
 
-  $graphite_host = $sensu::graphite_host
-  $graphite_port = $sensu::graphite_port
+  $graphite_host  = $sensu::graphite_host
+  $graphite_port  = $sensu::graphite_port
+  $sensu_interval = $sensu::sensu_interval
 
   file {
 	"/etc/sensu/conf.d/relay.json":
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class sensu ($graphite_host="localhost", $graphite_port="2003"){
+class sensu ($graphite_host="localhost", $graphite_port="2003", $sensu_interval="10"){
 
   include sensu::packages
   include sensu::repository
