5ee60190efc06c8d651fd1edc35ccea524a57ff8
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-16 20:52:11 +0200

    issue with cw in vi
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/client/service.pp
+++ manifests/client/service.pp
@@ -1,7 +1,7 @@
 class sensu::client::service {
 
    service {
-	"sensu-cwclientserver":
+	"sensu-client":
 		 enable => true,
 		 ensure => running,
 		 subscribe => File["/etc/sensu/conf.d/client.json"] 
