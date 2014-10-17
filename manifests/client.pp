fedb2c3305d7359fea7eb0f735ed1206eecb1840
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-17 15:22:01 +0200

    add missing includes
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,5 +1,8 @@
 class sensu::client ($redis_host="localhost", $rabbitmq_host="localhost", $rabbitmq_password=$rabbitmq::config::sensu::rabbitmq_sensu_password) {
 
+  include sensu::client::config
+  include sensu::client::service
+
   package {
 	"sensu-plugin":
 		ensure   => present,
