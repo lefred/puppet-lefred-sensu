c7db824ce26c5c4707dea4284980a0f311f07fd6
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-17 15:28:38 +0200

    add include for server classes
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,5 +1,8 @@
 class sensu::server ($graphite_host="localhost", $graphite_port="2003") {
 
+  include sensu::server::config
+  include sensu::server::service
+
   Class['sensu']->Class['sensu::server::config']->Class['sensu::server::service']
 
 }
