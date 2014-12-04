9c7ad2cdfe3486a8f194bd124f408aa9cf8d34f1
Frederic Descamps <lefred.descamps@gmail.com>
2014-12-04 15:34:12 +0100

    add info about release
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/client/mysql.pp
+++ manifests/client/mysql.pp
@@ -13,7 +13,7 @@ class sensu::client::mysql ($username=root, $sensu_interval=$sensu::interval, $p
                 content => template("sensu/mysql.json.erb"),
 		notify  => Service['sensu-client'];
   }
-  info("FREEEED  !!")
+  info("FREEEED $::osfamily  !!")
 
   case $::osfamily {
         'RedHat': {
